f508b1fae71e8df58a603ee13f45fdef06174950
EranMani <eranmanim@gmail.com>
2021-06-21 06:27:12 -0700

    Bit mask challenge using keys and doors

--- Assets/Scripts/AttributeManager.cs
+++ Assets/Scripts/AttributeManager.cs
@@ -6,67 +6,77 @@ using System;
 
 public class AttributeManager : MonoBehaviour
 {
-    static public int MAGIC = 16;
-    static public int INTELLIGENCE = 8;
-    static public int CHARISMA = 4;
-    static public int FLY = 2;
-    static public int INVISIBLE = 1;
+    static public int MAGIC_KEY = 16;
+    static public int INTELLIGENCE_KEY = 8;
+    static public int CHARISMA_KEY = 4;
+    static public int FLY_KEY = 2;
+    static public int INVISIBLE_KEY = 1;
 
-    public Text attributeDisplay;
-    public int attributes = 0;
+    public Text keysDisplay;
+    public int keys = 0;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "MAGIC")
+        switch (other.gameObject.tag)
         {
-            attributes |= MAGIC;
-        }
-        else if (other.gameObject.tag == "INTELLIGENCE")
-        {
-            attributes |= INTELLIGENCE;
-        }
-        else if (other.gameObject.tag == "CHARISMA")
-        {
-            attributes |= CHARISMA;
-        }
-        else if (other.gameObject.tag == "FLY")
-        {
-            attributes |= FLY;
-        }
-        else if (other.gameObject.tag == "INVISIBLE")
-        {
-            attributes |= INVISIBLE;
-        }
-        else if (other.gameObject.tag == "ANTIMAGIC")
-        {
-            attributes &= ~MAGIC;
-        }
-        else if (other.gameObject.tag == "MULTADD")
-        {
-            attributes |= (INTELLIGENCE | MAGIC | CHARISMA);
-        }
-        else if (other.gameObject.tag == "MULTREMOVE")
-        {
-            attributes &= ~(INTELLIGENCE | MAGIC);
-        }
-        else if (other.gameObject.tag == "RESET")
-        {
-            attributes = 0;
-        }
+            case "MAGIC":
+                keys |= MAGIC_KEY;
+                Destroy(other.gameObject);
+                break;
+            case "INTELLIGENCE":
+                keys |= INTELLIGENCE_KEY;
+                Destroy(other.gameObject);
+                break;
+            case "CHARISMA":
+                keys |= CHARISMA_KEY;
+                Destroy(other.gameObject);
+                break;
+            case "FLY":
+                keys |= FLY_KEY;
+                Destroy(other.gameObject);
+                break;
+            case "INVISIBLE":
+                keys |= INVISIBLE_KEY;
+                Destroy(other.gameObject);
+                break;
+            case "GOLD":
+                keys |= (MAGIC_KEY | INTELLIGENCE_KEY | CHARISMA_KEY | FLY_KEY | INVISIBLE_KEY);
+                Destroy(other.gameObject);
+                break;
+        }      
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 screenPoint = Camera.main.WorldToScreenPoint(this.transform.position);
-        attributeDisplay.transform.position = screenPoint + new Vector3(0,-50,0);
-        attributeDisplay.text = Convert.ToString(attributes, 2).PadLeft(8, '0');
+        keysDisplay.transform.position = screenPoint + new Vector3(0,-50,0);
+        keysDisplay.text = Convert.ToString(keys, 2).PadLeft(8, '0');
     }
-       
+
+ 
+
+
+  
+
+    /*
+       else if (other.gameObject.tag == "ANTIMAGIC")
+       {
+           attributes &= ~MAGIC;
+       }
+       else if (other.gameObject.tag == "MULTADD")
+       {
+           attributes |= (INTELLIGENCE | MAGIC | CHARISMA);
+       }
+       else if (other.gameObject.tag == "MULTREMOVE")
+       {
+           attributes &= ~(INTELLIGENCE | MAGIC);
+       }
+       else if (other.gameObject.tag == "RESET")
+       {
+           attributes = 0;
+       }
+       */
+
 }
--- Assets/Scripts/DoorManager.cs
+++ Assets/Scripts/DoorManager.cs
@@ -1,34 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class DoorManager : MonoBehaviour
 {
-    int doorType = AttributeManager.MAGIC;
+    public enum DoorType { MAGIC_DOOR, INVISIBLE_DOOR, FLY_DOOR, INTELLIGENCE_DOOR, CHARISMA_DOOR, FINAL_DOOR};
+
+    // The current state of enemy
+    public DoorType currentDoor = DoorType.MAGIC_DOOR;
+    int doorAttr = 0;
+
+    private void Start()
+    {
+        switch (currentDoor)
+        {
+            case DoorType.MAGIC_DOOR:
+                doorAttr = AttributeManager.MAGIC_KEY;
+                break;
+            case DoorType.INVISIBLE_DOOR:
+                doorAttr = AttributeManager.INVISIBLE_KEY;
+                break;
+            case DoorType.FLY_DOOR:
+                doorAttr = AttributeManager.FLY_KEY;
+                break;
+            case DoorType.INTELLIGENCE_DOOR:
+                doorAttr = AttributeManager.INTELLIGENCE_KEY;
+                break;
+            case DoorType.CHARISMA_DOOR:
+                doorAttr = AttributeManager.CHARISMA_KEY;
+                break;
+            case DoorType.FINAL_DOOR:
+                doorAttr = AttributeManager.INTELLIGENCE_KEY | AttributeManager.CHARISMA_KEY;
+                break;
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        // Use & bit operator to find if mask is matched to current attributes binary
-        if ((collision.gameObject.GetComponent<AttributeManager>().attributes & doorType) != 0)
+        int keys = collision.gameObject.GetComponent<AttributeManager>().keys;
+        print(Convert.ToString(keys, 2).PadLeft(8, '0'));
+        print(Convert.ToString(doorAttr, 2).PadLeft(8, '0'));
+        print(Convert.ToString(keys & doorAttr, 2).PadLeft(8, '0'));
+
+        // Check if the keys binary is the exact binary as the bit mask doorAttr
+        if ((keys & doorAttr) == doorAttr)
         {
-            this.GetComponent<BoxCollider>().isTrigger = true;
+            gameObject.GetComponent<BoxCollider>().isTrigger = true;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        this.GetComponent<BoxCollider>().isTrigger = false;
+        other.GetComponent<AttributeManager>().keys &= ~doorAttr;
+        gameObject.GetComponent<BoxCollider>().isTrigger = false;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
