79b21cfa1e72e1e1fb1eb7796d3b5aeb26812604
EranMani <eranmanim@gmail.com>
2021-06-26 10:18:13 -0700

    Move object by adding a new vector values to current position

--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -5,25 +5,13 @@ using System.Collections;
 
 public class Drive : MonoBehaviour
 {
-    public float speed = 10.0f;
-    public float rotationSpeed = 100.0f;
+    Vector2 direction = new Vector2(0.1f, 0.1f);
 
     void Update()
     {
-        // Get the horizontal and vertical axis.
-        // By default they are mapped to the arrow keys.
-        // The value is in the range -1 to 1
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-
-        // Make it move 10 meters per second instead of 10 meters per frame...
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
-
-        // Move translation along the object's z-axis
-        transform.Translate(0, translation, 0);
-
-        // Rotate around our y-axis
-        transform.Rotate(0, 0, -rotation);
+        Vector3 position = transform.position;
+        position.x += direction.x;
+        position.y += direction.y;
+        transform.position = position;
     }
 }
