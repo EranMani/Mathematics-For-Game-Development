fdf91f22e2a60d0c9ca9c310e31e6651b2dbd094
Eran Mani <eranmanim@gmail.com>
2021-06-28 16:18:58 +0300

    Imported project assets to test dot product functionality

---
+++ Assets/Scripts/Drive.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drive : MonoBehaviour
+{
+    public float speed = 0.01f;
+    public GameObject fuel;
+    Vector3 direction;
+    float stoppingDistance = 0.1f;
+
+    private void Start()
+    {
+        // Declare the vector direction from the object to the destination
+        direction = fuel.transform.position - transform.position;
+        Coords dirNormal = HolisticMath.GetNormal(new Coords(direction));
+        direction = dirNormal.ToVector();
+    }
+
+    void Update()
+    {
+        // Check for distance and update position with vector while in distance
+        if (HolisticMath.Distance(new Coords(transform.position), new Coords(fuel.transform.position)) > stoppingDistance)
+        {
+            transform.position += direction * speed * Time.deltaTime;
+        }
+
+        /*if (Vector3.Distance(transform.position, fuel.transform.position) > stoppingDistance)
+        {
+            transform.position += direction * speed;
+        }*/
+
+        // -- By calculating direction vector each frame, its magnitude will be reduced each frame and that will create
+        // -- the slow in and slow out effect - when the object is close to the destination point it will move slower
+        // -- and slower and he reached it
+        // Vector3 direction = fuel.transform.position - transform.position;
+        // transform.position += direction * speed;
+    }
+}
